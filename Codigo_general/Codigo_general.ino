032c71f85c60b347972ae0936788506e78bb0214
AlejandroSzew <49006858@est.ort.edu.ar>
2026-04-17 11:20:18 -0300

    Update Codigo_general.ino

--- Codigo_general/Codigo_general.ino
+++ Codigo_general/Codigo_general.ino
@@ -1,6 +1,7 @@
 #include "I2Cdev.h"
 #include "MPU6050.h"
 #include "Wire.h"
+#include "Wifi.h"
 typedef enum { RST,
                INICIALIZACION,
                MEDICIONES,
@@ -9,26 +10,36 @@ typedef enum { RST,
                ANALISIS_SERIE,
                C_APLICACION } estadoMaq_General_t;
 estadoMaq_General_t estadoMaq_General = RST;
+void medicionesEstandar();
+void mediciones();
 
-int ax, ay, az;
-int gx, gy, gz;
-#define PIN_BOTON = 
+float ax, ay, az;
+float gx, gy, gz;
+#define PIN_BOTON =
 #define PIN_LED_R =
 #define PIN_LED_G =
-#define PIN_LED_B = 
+#define PIN_LED_B =
+
+const char* ssid = "ESP32_C3_Server";
+const char* password = "GRUPO3";
+WebServer server(80);
 
 void setup() {
-  pinMode(PIN_BOTON,INPUT);
+  pinMode(PIN_BOTON, INPUT);
   pinMode(PIN_LED_R, OUTPUT);
   pinMode(PIN_LED_G, OUTPUT);
   pinMode(PIN_LED_B, OUTPUT);
-  
+  server.begin();
   Serial.begin(57600);  //Iniciando puerto serial
   Wire.begin();         //Iniciando I2C
   sensor.initialize();  //Iniciando el sensor
-  
-  if (sensor.testConnection()) { Serial.println("Sensor iniciado correctamente"); }
-  else { Serial.println("Error al iniciar el sensor"); }
+  WiFi.softAP(ssid, password);
+  IPAddress ip = Wifi.soft if (sensor.testConnection()) {
+    Serial.println("Sensor iniciado correctamente");
+  }
+  else {
+    Serial.println("Error al iniciar el sensor");
+  }
 }
 
 
@@ -42,12 +53,76 @@ void Maq_General() {
 
 
     case MEDICIONES:
-    sensor.getAcceleration(&ax, &ay, &az); //aceleracion del sensor en x, y, z
-    sensor.getRotation(&gx, &gy, &gz); //angulo del sensor en x, y, z
+      medicionesEstandar();
+      mediciones();
+
 
     case C_WIFI:
+    //datoss al sensro principal
     case ANALISIS_REP:
+    //Azul
+    //digitalWrite(PIN_LED_R,0);
+    //digitalWrite(PIN_LED_G,0);
+    //digitalWrite(PIN_LED_B,255);
+
+    //Verde
+    //digitalWrite(PIN_LED_R,0);
+    //digitalWrite(PIN_LED_G,255);
+    //digitalWrite(PIN_LED_B,0);
+
+    //Rojo
+    //digitalWrite(PIN_LED_R,255);
+    //digitalWrite(PIN_LED_G,0);
+    //digitalWrite(PIN_LED_B,0);
+
+    //analizan valores, devolucion led, se guardan los errores, apreto el boton termina la serie
     case ANALISIS_SERIE:
+    //analisis errores generales, apreto boton 5s apaga sistema
     case C_APLICACION:
   }
+}
+
+
+void medicionesEstandar() {
+  sensor.getAcceleration(&ax, &ay, &az);  //aceleracion del sensor en x, y, z
+  sensor.getRotation(&gx, &gy, &gz);      //angulo del sensor en x, y, z
+  int valores_estandar_ax = ax * 9.81;
+  int valores_estandar_ay = ay * 9.81;
+  int valores_estandar_az = az * 9.81;
+
+  int valores_estandar_gx = gx int valores_estandar_gy = gy int valores_estandar_gz = gz
+  Serial.println("valor estandar ax en (m/s^2)", valores_estandar_ax);
+  Serial.println("valor estandar ay en (m/s^2)", valores_estandar_ay);
+  Serial.println("valor estandar az en (m/s^2)", valores_estandar_az);
+
+  Serial.println("valor estandar gx en (°/s)", valores_estandar_gx);
+  Serial.println("valor estandar gy  en (°/s)", valores_estandar_gy);
+  Serial.println("valor estandar gz en (°/s)", valores_estandar_gz);
+}
+
+void mediciones() {
+  sensor.getAcceleration(&ax, &ay, &az);  //aceleracion del sensor en x, y, z
+  sensor.getRotation(&gx, &gy, &gz);      //angulo del sensor en x, y, z
+  float valores_ax = ax * 9.81;
+  float valores_ay = ay * 9.81;
+  float valores_az = az * 9.81;
+
+  float valores_gx = gx;
+  float valores_gy = gy;
+  float valores_gz = gz;
+/*float diff_ax = valores_ax - valores_estandar_ax;
+float  diff_ay = valores_ay - valores_estandar_ay;
+float  diff_az = valores_az - valores_estandar_az;
+
+float  diff_gx = valores_gx - valores_estandar_gx;
+float  diff_gy = valores_gy -  valores_estandar_gx;
+float  diff_gz = valores_gz -  valores_estandar_gx;
+*/
+  Serial.println("valores ax en (m/s^2)", valores_ax);
+  Serial.println("valor  ay en (m/s^2)", valores_ay);
+  Serial.println("valor  az en (m/s^2)", valores_az);
+
+  Serial.println("valor  gx en (°/s)", valores_gx);
+  Serial.println("valor  gy  en (°/s)", valores_gy);
+  Serial.println("valor  gz en (°/s)", valores_gz);
 }
