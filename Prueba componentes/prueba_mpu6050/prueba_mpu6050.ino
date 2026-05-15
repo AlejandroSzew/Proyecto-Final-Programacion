b11a3d7b10af42cfe57bd7a41b2f7a7700703777
Android Studio <devandroid@ort>
2026-05-15 09:26:03 -0300

    cambie la prueba del mpu6050 a adxl345 y funciona

--- Prueba componentes/prueba_mpu6050/prueba_mpu6050.ino
+++ Prueba componentes/prueba_mpu6050/prueba_mpu6050.ino
@@ -1,62 +1,37 @@
-#include "Wire.h"
-#include "I2Cdev.h"
-#include "MPU6050.h"
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
 
-MPU6050 sensor;
-
-float ax, ay, az;
-float gx, gy, gz;
-
-float ax_ms2, ay_ms2, az_ms2;
-float inclX, inclY, inclZ;
+// Crear objeto del sensor
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 
 void setup() {
   Serial.begin(115200);
   Wire.begin();
 
-  sensor.initialize();
-
-  if (sensor.testConnection()) {
-    Serial.println("MPU6050 conectado correctamente ");
-  } else {
-    Serial.println("Error al conectar el MPU6050 ");
+  if (!accel.begin()) {
+    Serial.println("Error al conectar el ADXL345");
+    while (1);
   }
+  Serial.println("ADXL345 conectado correctamente");
+
+  // Configurar rango de medida
+  accel.setRange(ADXL345_RANGE_2_G);  // ±2g
 }
 
 void loop() {
-  // Lectura cruda
-  sensor.getAcceleration(&ax, &ay, &az);
-  sensor.getRotation(&gx, &gy, &gz);
+  sensors_event_t event;
+  accel.getEvent(&event);
 
-  Serial.print("RAW Accel: ");
-  Serial.print(ax); Serial.print(", ");
-  Serial.print(ay); Serial.print(", ");
-  Serial.println(az);
-
-  Serial.print("RAW Gyro: ");
-  Serial.print(gx); Serial.print(", ");
-  Serial.print(gy); Serial.print(", ");
-  Serial.println(gz);
-
-  // Conversión a m/s²
-  ax_ms2 = (ax / 16384.0) * 9.81;
-  ay_ms2 = (ay / 16384.0) * 9.81;
-  az_ms2 = (az / 16384.0) * 9.81;
-
-  // Inclinaciones
-  inclX = atan2(ax_ms2, sqrt(ay_ms2*ay_ms2 + az_ms2*az_ms2)) * 180.0 / PI;
-  inclY = atan2(ay_ms2, sqrt(ax_ms2*ax_ms2 + az_ms2*az_ms2)) * 180.0 / PI;
-  inclZ = atan2(az_ms2, sqrt(ax_ms2*ax_ms2 + ay_ms2*ay_ms2)) * 180.0 / PI;
+  // Lectura en m/s² directamente
+  float ax_ms2 = event.acceleration.x;
+  float ay_ms2 = event.acceleration.y;
+  float az_ms2 = event.acceleration.z;
 
   Serial.print("Accel (m/s²): ");
   Serial.print(ax_ms2); Serial.print(", ");
   Serial.print(ay_ms2); Serial.print(", ");
   Serial.println(az_ms2);
-
-  Serial.print("Inclinaciones (°): ");
-  Serial.print(inclX); Serial.print(", ");
-  Serial.print(inclY); Serial.print(", ");
-  Serial.println(inclZ);
 
   Serial.println("-----------------------------");
   delay(1000);
